2863c158c7d387770a3b1e000edf0bd6452f58ae
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-02-19 12:39:57 -0500

    meet MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import { connect } from 'react-redux' // HOC
+import { removeFeature } from '../actions/removeFeatureAction'
 const AddedFeature = props => {
+  console.log('this is second props',props )
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
+const mapStateToProps = (state) => {
+  return {
+     key:state.additionalFeatures.id,
+  }
+}
 
-export default AddedFeature;
+export default connect(mapStateToProps,{removeFeature})(AddedFeature)
